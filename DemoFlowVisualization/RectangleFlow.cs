e8f0ebe163003eb703b5c6c043514966f7105f00
glckfndr <obulan65@gmail.com>
2021-09-25 17:16:29 +0300

    Add SetAll and other method in GeomatryShape

--- DemoFlowVisualization/RectangleFlow.cs
+++ DemoFlowVisualization/RectangleFlow.cs
@@ -35,6 +35,7 @@ namespace DemoFlowVisualization
         private VertexArray _particleVAO;
         private mat4 _view;
         private mat4 _model;
+        private mat4 _proj;
         private float _eyePos = 10.0f;
         private float _angle = 0;
         private int Height;
@@ -56,44 +57,7 @@ namespace DemoFlowVisualization
             Height = height;
             Width = width;
 
-            GeometryShape polygon = new GeometryShape(new List<Vector>
-            {
-                new Vector(-0.7,-0.5),
-                new Vector(-0.7,0.4),
-                new Vector(0.2,0.6),
-                new Vector(0.5,0.0),
-                new Vector(0.5,-0.6)
-                
-                
-                //new Vector(0.3,0.0),
-                //new Vector(0.25,-0.1)
-
-            }, true);
-            _bogyTriangles = GetVertex(polygon.Triangulation);
-
-            GeometryShape polygon1 = new GeometryShape(new List<Vector>
-            {
-
-                new Vector(1.2,-0.6),
-                new Vector(1.9,0.5),
-                new Vector(2.1,-0.4),
-                new Vector(1.9,-1.6),
-                //new Vector(0.25,-0.1)
-
-            }, true);
-
-            _bogyTriangles.AddRange(GetVertex(polygon1.Triangulation));
-
-
-            GeometryShapeCollection sp = new GeometryShapeCollection();
-            sp.AddShape(polygon);
-            sp.AddShape(polygon1);
-            var ds = new Discretizator(0.1, 0.1);
-            _vortexSystem = new VortexSystem(sp, ds, new RankinVortex());
-            _vortexSystem.FlowVelocity = _flowVelovity;
-            _vortexSystem.FirstStep();
-
-
+            SetGeometryAndVortexSystem();
 
             _particleNumber = (int)(_nParticles.x * _nParticles.y * _nParticles.z);
             SetOpenGlParameters();
@@ -104,6 +68,81 @@ namespace DemoFlowVisualization
             _vortexStructArray = _vortexesFromMemory.GetArrayOfVortexStruct();
             _vortexBuf.SubData(_vortexStructArray);
         }
+
+        private void SetGeometryAndVortexSystem()
+        {
+            var sp = CreatePolygons();
+            var ds = new Discretizator(0.1, 0.1);
+            _vortexSystem = new VortexSystem(sp, ds, new RankinVortex());
+            _vortexSystem.FlowVelocity = _flowVelovity;
+            _vortexSystem.FirstStep();
+        }
+
+        private GeometryShapeCollection CreatePolygons()
+        {
+
+            var polygonPoints = new List<Vector>
+            {
+                new Vector(-0.5, -0.5),
+                new Vector(-0.5, 0.5),
+                new Vector(0.5, 0.5),
+                new Vector(0.5, -0.5),
+
+            };
+            
+            _bogyTriangles = new List<Vector2>();
+            GeometryShapeCollection shapeCollection = new GeometryShapeCollection();
+
+            GeometryShape polygon = new GeometryShape(polygonPoints, true);
+            polygon.SetAll(new Vector(0.5,1.75), 30, new Vector(2.5, -0.75));
+            _bogyTriangles.AddRange(GetVertex(polygon.Triangulation));
+            shapeCollection.AddShape(polygon);
+
+            //GeometryShape polygon1 = new GeometryShape(new List<Vector>
+            //{
+            //    new Vector(1.2, -0.6),
+            //    new Vector(1.9, 0.5),
+            //    new Vector(2.1, -0.4),
+            //    new Vector(1.9, -1.6),
+            //    //new Vector(0.25,-0.1)
+            //}, true);
+
+            //GeometryShape polygon1 = new GeometryShape(polygonPoints, true);
+            polygon = new GeometryShape(polygonPoints, true);
+            polygon.SetAll(new Vector(0.6, 1.2), 0, new Vector(0, -0.75));
+            _bogyTriangles.AddRange(GetVertex(polygon.Triangulation));
+            shapeCollection.AddShape(polygon);
+
+
+            polygon = new GeometryShape(polygonPoints, true);
+            polygon.SetAll(new Vector(0.6, 1.2), 0, new Vector(0, 1.25));
+            _bogyTriangles.AddRange(GetVertex(polygon.Triangulation));
+            shapeCollection.AddShape(polygon);
+
+            polygon = new GeometryShape(polygonPoints, true);
+            polygon.SetAll(new Vector(0.5, 1.0), 30, new Vector(2, 1.5));
+            _bogyTriangles.AddRange(GetVertex(polygon.Triangulation));
+            shapeCollection.AddShape(polygon);
+            
+            return shapeCollection;
+        }
+
+        //private void SetNewHouse(GeometryShape  polygon, Vector scale, double rot, Vector offset)
+        //{
+        //    // polygon.ScaleX(0.5);
+        //    // polygon.ScaleY(1.75);
+        //    // polygon.Rotate(30);
+
+        //    // polygon.Move(new Vector(2.5, -0.75));
+
+        //    polygon.ScaleX(scale.X);
+        //    polygon.ScaleY(scale.Y);
+        //    polygon.Rotate(rot);
+
+        //    polygon.Move(offset);
+
+        //    _bogyTriangles.AddRange(GetVertex(polygon.Triangulation));
+        //}
 
         private List<Vector2> GetVertex(List<Triangle> polygonTriangulation)
         {
@@ -145,30 +184,17 @@ namespace DemoFlowVisualization
             // Set up the VAO
             _particleVAO = SetVAO(new[] { _particleBuf, _particleVelBuf }, new[] { 0, 1 }, new[] { 4, 4 });
             _vortexVAO = SetVAO(new[] { _vortexBuf }, new[] { 1 }, new[] { 4 });
-            //_vortexVAO = new VertexArray();
-            //  _vortexBuf.SetAttribPointer(1, 4);
-            //_vortexVAO.Unbind();
-
-            //_bodyBufferObject = new StorageBuffer(BufferUsageHint.DynamicDraw); ;
-            //_bodyBufferObject.SetData(_bogyTriangles.ToArray(), 6);
-
             _bodyVAO = SetVAO(new[] { _bodyBufferObject }, new[] { 6 }, new[] { 2 });
-            //_bodyVAO = new VertexArray();
-            //  _bodyBufferObject.SetAttribPointer(6, 2);
-            //_bodyVAO.Unbind();
+
         }
 
         private VertexArray SetVAO(StorageBuffer[] buffers, int[] ind, int[] num)
         {
             var vertexArray = new VertexArray();
-            //_particleBuf.SetAttribPointer(0, 4);
-            //_vortexBuf.SetAttribPointer(0,4);
-            //_particleVelBuf.SetAttribPointer(1, 4);
             for (int i = 0; i < buffers.Length; i++)
             {
                 buffers[i].SetAttribPointer(ind[i], num[i]);
             }
-
 
             vertexArray.Unbind();
             return vertexArray;
@@ -191,98 +217,120 @@ namespace DemoFlowVisualization
 
         private void CreateShaders()
         {
+            // шейдер для розрахунку руху частинок
             _particleComputeShader = new Shader("../../Shaders/particle.comp");
+            // шейдер для розрахунку руху вихорів
             _velocityComputeShader = new Shader("../../Shaders/Velocity2D.comp");
 
+            // шейдер для відображення частинок
             _particleShader = new Shader("../../Shaders/particles.vert",
                 "../../Shaders/particles.frag");
 
-
+            // шейдер для відображення вихорів текстурами
             _vortexShader = new Shader("../../Shaders/vortexTex.vert",
                 "../../Shaders/vortexTex.frag");
+            // шейдер для відображення тіл
             _bodyShader = new Shader("../../Shaders/body.vert",
                 "../../Shaders/body.frag");
         }
 
         public void ComputeAndDraw(bool isPause, bool is3D)
         {
-
-            if (_counter % 1 == 0)
+            if (_vortexStructArray.Length > 8200)
+                isPause = true;
+            if (!isPause)
             {
-                Console.WriteLine(_vortexSystem.Time);
-                _counter = 0;
-
-                //_vortexesFromMemory = new VortexesFromMemory(_vortexSystem);
-                //_vortexes = _vortexesFromMemory.GetArrayOfVortexStruct();
-                Console.WriteLine("Len : " + _vortexStructArray.Length);
-
-                // _vortexBuf.SubData(_vortexes, 1);
-                //_vortexBuf.SubData(_vortexes);
-                var freeVortexes = _vortexesFromMemory.GetListOfFreeVortex();
-
-                _velocityComputeShader.SetInt("vortexNumber", _vortexStructArray.Length);
-                _velocityComputeShader.SetInt("freeVortexNumber", freeVortexes.Count);
-                var startIndex = _vortexStructArray.Length - freeVortexes.Count;
-                _velocityComputeShader.SetInt("startFreeVortexNumber", startIndex);
-
-                _velocityComputeShader.SetVector4("flowVelocity",
-                                    new Vector4((float)_flowVelovity.X, (float)_flowVelovity.Y, 0, 1));
-                //_velocityBuffer.BindLayout(1);
-                //_vortexBuf.BindLayout(1);
-                _velocityComputeShader.Compute(1024 * 8 / 128, 1, 1);
-                var gpuVortexVelocities = _velocityBuffer.GetVector2Data();
-                for (var i = 0; i < freeVortexes.Count; i++)
+                if (_counter % 1 == 0)
                 {
-                    freeVortexes[i].InVelocity = new Vector(gpuVortexVelocities[i + startIndex].X, gpuVortexVelocities[i + startIndex].Y);
+                    Console.WriteLine(_vortexSystem.Time);
+                    _counter = 0;
+                  
+                    //   _vortexesFromMemory = new VortexesFromMemory(_vortexSystem);
+                    var freeVortexes = _vortexesFromMemory.GetListOfFreeVortex();
+                    var startIndex = ComputeVelocityWithShader(freeVortexes);
+                    SetVoretexVelocity(freeVortexes, startIndex);
+
+                    // робимо крок по часу методом дискретних вихорів
+                    //  _vortexSystem.NextStep();
+                    _vortexSystem.NextStepGPU();
+                    CopyVortexInGPU();
+                    Console.WriteLine("Len : " + _vortexStructArray.Length);
+                    
+                        
                 }
 
-                //  _vortexSystem.NextStep();
-                _vortexSystem.NextStepGPU();
-                _vortexesFromMemory = new VortexesFromMemory(_vortexSystem);
-                _vortexStructArray = _vortexesFromMemory.GetArrayOfVortexStruct();
-                Console.WriteLine("Len : " + _vortexStructArray.Length);
-                //_vortexBuf.SubData(_vortexes, 1);
-                _vortexBuf.SubData(_vortexStructArray);
+                _counter++;
+                ComputeParticlePosition();
             }
 
-            _counter++;
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            // Draw the particles - малювання частинок
+            SetMvpMatrix();
+            SetVisualShaders();
+            _particleShader.Use();
+            // _particleShader.SetVector4("Color", new Vector4(0.9f, 0.7f, 0, 0.8f));
+            _particleShader.SetVector4("Color", new Vector4(0.0f, 0.0f, 0, 0.8f));
+            GL.PointSize(2.0f);
+            _particleVAO.Draw(PrimitiveType.Points, 0, (int)(_nParticles.x * _nParticles.y * _nParticles.z));
 
+
+            // малювання вихорів текстурами
+            //_vortexShader.Use();
+            //_vortexVAO.Bind();
+            //GL.VertexAttribDivisor(1, 1);
+            //GL.DrawArraysInstanced(PrimitiveType.Triangles, 0, 6, _vortexStructArray.Length);
+            //_vortexVAO.Unbind();
+
+            // малювання тіл
+            _bodyShader.Use();
+            _bodyVAO.Draw(PrimitiveType.Triangles, 0, _bogyTriangles.Count);
+            // GL.PointSize(8.0f);
+            // _bodyVAO.Draw(PrimitiveType.Points, 0, _bogyTriangles.Count * 3);
+
+
+        }
+
+        private void CopyVortexInGPU()
+        {
+            _vortexesFromMemory = new VortexesFromMemory(_vortexSystem);
+            // формуємо структури вихорів із вихрової системи
+            _vortexStructArray = _vortexesFromMemory.GetArrayOfVortexStruct();
+            // копіюємо структури вихорів на відеокарту
+            _vortexBuf.SubData(_vortexStructArray);
+        }
+
+        private void SetVoretexVelocity(List<Vortex> freeVortexes, int startIndex)
+        {
+            var gpuVortexVelocities = _velocityBuffer.GetVector2Data();
+            for (var i = 0; i < freeVortexes.Count; i++)
+            {
+                freeVortexes[i].InVelocity =
+                    new Vector(gpuVortexVelocities[i + startIndex].X, gpuVortexVelocities[i + startIndex].Y);
+            }
+        }
+
+        private void ComputeParticlePosition()
+        {
             _particleComputeShader.SetInt("vortexNumber", _vortexStructArray.Length);
             _particleComputeShader.SetFloat("deltaTime", 0.05f);
             _particleComputeShader.SetVector4("flowVelocity",
                 new Vector4((float)_flowVelovity.X, (float)_flowVelovity.Y, 0, 1));
-
-            // _particleBuf.BindLayout(0);
-            //_vortexBuf.BindLayout(1);
-            //_particleVelBuf.BindLayout(2);
-            //_startPos.BindLayout(3);
-            //_lifeTimeBuf.BindLayout(4);
-
+            // обчислення позицій частинок
             _particleComputeShader.Compute(_particleNumber / 128, 1, 1);
+        }
 
-            SetParticleShader();
+        private int ComputeVelocityWithShader(List<Vortex> freeVortexes)
+        {
+            _velocityComputeShader.SetInt("vortexNumber", _vortexStructArray.Length);
+            _velocityComputeShader.SetInt("freeVortexNumber", freeVortexes.Count);
+            var startIndex = _vortexStructArray.Length - freeVortexes.Count;
+            _velocityComputeShader.SetInt("startFreeVortexNumber", startIndex);
 
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            _particleShader.Use();
-            // Draw the particles
-            _particleShader.SetVector4("Color", new Vector4(0.9f, 0.7f, 0, 0.8f));
-            GL.PointSize(1.0f);
-            _particleVAO.Draw(PrimitiveType.Points, 0, (int)(_nParticles.x * _nParticles.y * _nParticles.z));
+            _velocityComputeShader.SetVector4("flowVelocity",
+                new Vector4((float)_flowVelovity.X, (float)_flowVelovity.Y, 0, 1));
 
-            _vortexShader.Use();
-            //_vortexVAO.Bind();
-            //GL.VertexAttribDivisor(1, 1);
-            //GL.DrawArraysInstanced(PrimitiveType.Triangles, 0, 6, _vortexStructArray.Length);
-            // GL.PointSize(8.0f);
-            // _vortexVAO.Draw(PrimitiveType.Points, 0, _vortexes.Length);
-            //_vortexVAO.Unbind();
-
-            _bodyShader.Use();
-            GL.PointSize(8.0f);
-             _bodyVAO.Draw(PrimitiveType.Triangles,0,_bogyTriangles.Count);
-           // _bodyVAO.Draw(PrimitiveType.Points, 0, _bogyTriangles.Count * 3);
-
-
+            _velocityComputeShader.Compute(1024 * 8 / 128, 1, 1);
+            return startIndex;
         }
 
         public static List<Vector4> GetParticles(vec3 nParticles)
@@ -344,9 +392,6 @@ namespace DemoFlowVisualization
             return list;
         }
 
-
-
-
         private List<Vector4> GetInitialVelocity(vec3 nParticles)
         {
             List<Vector4> lst = new List<Vector4>();
@@ -388,27 +433,22 @@ namespace DemoFlowVisualization
 
         private void SetOpenGlParameters()
         {
-            GL.ClearColor(0, 0.0f, 0.0f, 1);
+            GL.ClearColor(1, 1.0f, 1.0f, 1);
             GL.Enable(EnableCap.DepthTest);
             GL.Enable(EnableCap.Blend);
             GL.PointSize(1.0f);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
         }
 
-        private void SetParticleShader()
+        private void SetVisualShaders()
         {
-            //_angle = 90;
-            _view = glm.lookAt(new vec3(_xCenter, 0, _eyePos), new vec3(_xCenter, 0, 0), new vec3(0, 1, 0));
-            _model = glm.rotate(new mat4(1.0f), glm.radians(_angle), new vec3(1, 0.0f, 0.0f));
-            mat4 proj = glm.perspective(glm.radians(60.0f), (float)Width / Height, 1.0f, 100.0f);
-
             var model = _model.ConvertToMatrix4();
             var view = _view.ConvertToMatrix4();
-            var projection = proj.ConvertToMatrix4();
+            var projection = _proj.ConvertToMatrix4();
+
             _particleShader.SetMatrix4("model", model);
             _particleShader.SetMatrix4("view", view);
             _particleShader.SetMatrix4("projection", projection);
-
 
             _vortexShader.SetMatrix4("model", model);
             _vortexShader.SetMatrix4("view", view);
@@ -417,7 +457,13 @@ namespace DemoFlowVisualization
             _bodyShader.SetMatrix4("model", model);
             _bodyShader.SetMatrix4("view", view);
             _bodyShader.SetMatrix4("projection", projection);
+        }
 
+        private void SetMvpMatrix()
+        {
+            _view = glm.lookAt(new vec3(_xCenter, 0, _eyePos), new vec3(_xCenter, 0, 0), new vec3(0, 1, 0));
+            _model = glm.rotate(new mat4(1.0f), glm.radians(_angle), new vec3(1, 0.0f, 0.0f));
+            _proj = glm.perspective(glm.radians(60.0f), (float)Width / Height, 1.0f, 100.0f);
         }
 
         public void SetEye(float eyePos)
--- DemoFlowVisualization/Shaders/particles.frag
+++ DemoFlowVisualization/Shaders/particles.frag
@@ -13,12 +13,15 @@ void main()
 	float k2 = step(0.6, col)*step(col, 0.8);
 	float k3 = step(0.8, col)*step(col, 1.0);
 	
-	vec4 color3 = vec4(0.5, 0.0, 0, 0.5 + col);
-	vec4 color1 = Color*col;
-	color1 = vec4(0.0, 0.4 + col, 0.0, 0.7);
+	//vec4 color3 = vec4(0.5, 0.0, 0, 0.5 + col);
+	vec4 color1 = Color + vec4(0.0, 0.5 + col, 0.0, 0.8);
+	vec4 color2 = vec4(0.0, 0, 0.5 + col, 0.9);
+	vec4 color3 = vec4(0.5 + col, 0, 0, 1);
+	//vec4 color1 = Color*col;
 	
-	vec4 color2 = vec4(0.0, 0, 0.2 + col, 0.7);
-	vec4 color4 = vec4(col, col*0.5, 0.0, 1);
+	
+	
+	vec4 color4 = vec4(col + 0.5, 0.0, col, 1);
 	FragColor = k1*color1 + (1-k1)*k2*color2 + (1-k2)*k3*color3 + (1-k1)*(1-k2)*(1-k3)*color4;
 	FragColor.a = sin(absVelocity.y * 3.141);
 	//FragColor = Color;
